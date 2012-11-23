71107dfaf882fc9a0d2fc17c3ef7f03676e9a4af
Alvaro F. Lara <alvarola@gmail.com>
2012-11-23 00:56:00 -0300

    Modified js to be a little more modular

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source "http://rubygems.org/"
 gem 'sinatra'
 gem 'json'
 gem 'thin'
-gem "data_mapper", "1.2.0"
+gem 'data_mapper', "1.2.0"
 gem 'dm-postgres-adapter', "1.2.0"
 gem 'pg', "0.13.2"
 gem 'rack-google-analytics'
--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,36 @@
 var eventSource = new EventSource("/stream");
 
-eventSource.onmessage = function(e) {
-  response = jQuery.parseJSON(e.data);
-  console.log(response);
-  switch(response.type)
-  {
-    case 0: $("#user_list").append("<li id='"+response.message.user_logged_in.username+"'><a href='#'>" +response.message.user_logged_in.username+ "</a></li>");
-    break;
-    
-    case 1: $("#"+response.message.user_logged_out.username).remove();
-    break;
-    
-    case 2: $("#chat").append("<p><strong>"+ response.message.owner +":  </strong>"+ response.message.body +"<span class='pull-right'>"+ response.message.created_at +"</span></p>");
+$(document).ready(function(){
+  
+  $(document).bind("logout", function(event, response) { $("#"+response.message.user_logged_out.username).remove(); });
+  $(document).bind("login",  function(event, response) { $("#user_list").append("<li id='"+response.message.user_logged_in.username+"'><a href='#'>" +response.message.user_logged_in.username+ "</a></li>"); });
+  $(document).bind("update", function(event, response) {
+    $("#chat").append("<p><strong>"+ response.message.owner +":  </strong>"+ response.message.body +"<span class='pull-right'>"+ response.message.created_at +"</span></p>");
     $("#chat").animate({ scrollTop: $(document).height() }, "slow");
-		break;
-  }
-}
+  });
+  
+  eventSource.addEventListener("message", function(e) {
+    $(document).trigger("")
+    
+    response = jQuery.parseJSON(e.data);
+    
+    switch(response.type)
+    {
+      case 0: $(document).trigger("login", response);
+      break;
+    
+      case 1: $(document).trigger("logout", response);
+      break;
+    
+      case 2: $(document).trigger("update", response);
+  		break;
+    }
+  });
 
-$("form").live("submit", function(e) {
-    $.post('/say', {message: $('#message_input').val()});
-    $('#message_input').val(''); $('#message_input').focus();
-		$("#chat").animate({ scrollTop: $(document).height() }, "slow");    
-		e.preventDefault();
+  $("form").live("submit", function(e) {
+      $.post('/say', {message: $('#message_input').val()});
+      $('#message_input').val(''); $('#message_input').focus();
+  		$("#chat").animate({ scrollTop: $(document).height() }, "slow");    
+  		e.preventDefault();
+  });
 });
